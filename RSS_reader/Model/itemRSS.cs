d920ac9e790f9ead69ca00384de3fa074e0c4af3
Mrowiec Marek <HotimPL@gmail.com>
2020-03-11 10:43:24 +0100

    ADD itemRSS jako model itemu pobieranego z XML'a oraz RssReader jako klasa przechowująca listę Itemów z RSS'a oraz metodę do zczytywania danego rss'a po linku

---
+++ RSS_reader/Model/itemRSS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_reader.Model
+{
+    class itemRSS
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string Parent { get; set; }
+        public string Comments { get; set; }
+        public string Guid { get; set; }
+        public List<string> Categories = new List<string>();
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+
+        public DateTime GetDatatime()
+        {
+            var time = new DateTime();
+
+            time = DateTime.Parse(this.PubDate);
+
+            return time;
+        }
+        public string GetDescriptionText()
+        {
+            string descriptionText = "";
+
+            var descriptionSplit = this.Description.Split('>');
+            descriptionText = descriptionSplit[descriptionSplit.Length - 1];
+
+            return descriptionText;
+        }
+    }
+}
