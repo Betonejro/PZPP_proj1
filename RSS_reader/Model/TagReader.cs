1d5b47ffb575c095c047d1bd8cfa97d522058249
Krutek Krutkowski <Krutek@interia.eu>
2020-03-23 01:01:32 +0100

    Poprawiam co zniszczyłem

--- RSS_reader/Model/TagReader.cs
+++ RSS_reader/Model/TagReader.cs
@@ -19,6 +19,7 @@ namespace RSS_reader.Model
         
         public void ReadTags()
         {
+      
             var url = "https://media2.pl/rss";
             var web = new HtmlWeb();
             var doc = web.Load(url);
@@ -29,16 +30,16 @@ namespace RSS_reader.Model
             {
                 htmlLinks.AppendLine(current.Attributes["href"].Value);
             }
-
+          
             var document = htmlLinks.ToString();
             var newLine = new[] { '\r', '\n' };
             var tags = document.Split(newLine, StringSplitOptions.RemoveEmptyEntries);
-
+           
             foreach (var item in tags)
             {
                 var tail = item.Substring(item.Length - 4);
 
-                if (tail != ".xml") continue;
+                if (tail != ".xml") return;
 
                 var itemSplit = item.Split('/');
                 var itemTail = itemSplit[itemSplit.Length-1];
@@ -48,17 +49,20 @@ namespace RSS_reader.Model
                 Tags.Add(currentTag);
 
             }
+           
             SaveToDB();
+         
         }
 
         private void SaveToDB()
         {
             const string sourcesCollection = "XML_sources";
-            var db = new MongoCRUD("BazaTestowa");
+            var db = new MongoCRUD("NowaBaza");
 
             foreach (var sources in Tags)
             {
-                if (db.CheckRecordInCollection(sourcesCollection,sources.Name) != true)
+                
+                if (db.CheckRecordInCollection(sourcesCollection,sources.Name) == true)
                 {
                     db.InsertSourceToDatabase(sourcesCollection, sources);
                 }
